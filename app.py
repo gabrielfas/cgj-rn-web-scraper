89fac30aa1da59b2b1c92550d43ac0839aea7f6f
Gabriel Sousa <g.felipes@hotmail.com>
2019-11-19 08:15:56 -0300

    Deixando repositório em português

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Import packages
+# Importando pacotes
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -9,16 +9,17 @@ import sys
 
 warnings.filterwarnings('ignore')
 
-# Getting the links inside the tags <a>
+# Recuperando os links dentro das tags <a>
 def get_links(a_tags):
 	print('Recuperando links...')
 	links = []
-	# Appending the links to a list
+	# Armazenando os links em uma lista
 	for link in a_tags:
 		links.append(link.get('href'))
+        
 	return links
 
-# Getting the data from the links
+# Recuperando os dados ao acessar os links
 def get_dados(links):
 	print('Coletando dados...')
 	dados = []
@@ -44,52 +45,50 @@ def get_dados(links):
 
 	return dados
 
-# Exporting the data into a csv file
+# Exportando os dados
 def exportar_dados(dados, tipo):
 	print('Exportando dados...')
-	# Converting data into a pandas object
+	# Convertendo os dados de lista para dataframe
 	df_comarcas = pd.DataFrame(dados, columns=['UNIDADE', 'CHEFE DE SECRETARIA', 'TELEFONE', 'COMARCA'])
-	# Checking which file extension to export
+	# Verificando a flag para saber qual o tipo do arquivo para exportar
 	if tipo == 1:
-		df_comarcas.to_excel('outputs/Dados Comarcas TJRN.xlsx', index=False)
+		df_comarcas.to_excel('arquivos/Dados Comarcas TJRN.xlsx', index=False)
 	else:
-		df_comarcas.to_csv('outputs/Dados Comarcas TJRN.csv', index=False)
+		df_comarcas.to_csv('arquivos/Dados Comarcas TJRN.csv', index=False)
 	print('Dados exportados com sucesso!!!')
 
 if __name__ == "__main__":
 	print('Iniciando scraping...')
 
-	# Specify url
+	# Especificando url
 	url = 'https://corregedoria.tjrn.jus.br/index.php/judicial/mapa/mapa-judiciario'
 
-	# Package the request, send the request and catch the response: r
+	# Inicializando a requisição ao site
 	r = requests.get(url, verify=False)
 
-	# Extracts the response as html: html_doc
+	# Extraindo a resposta como html: html_doc
 	html_doc = r.text
 
-	# create a BeautifulSoup object from the HTML: soup
+	# Criando um objeto BeautifulSoup a partir do HTML: soup
 	soup = BeautifulSoup(html_doc)
 
-	# Find all 'a' tags (which define hyperlinks): a_tags
+	# Encontrando todos as tags 'a': a_tags
 	a_tags = soup.find_all('a')
 
-	# Calling get_links function
+	# Chamando a função para recuperar os links da página
 	links = get_links(a_tags)
 
-	# Slicing all links to get only the cities links
+	# Pegando apenas os links de cidades
 	links_comarcas = links[links.index('/index.php/acari'):links.index('/index.php/upanema')+1]
 
-	#print(links_comarcas)
-
-	# Calling get_dados function
+	# Chamando a função para trazer os dados do site
 	dados_comarcas = get_dados(links_comarcas)
 	
-	# Checking if command line is null
+	# Checando se a flag é nula
 	if len(sys.argv) == 1:
 		tipo_arquivo = 0
 	else:
 		tipo_arquivo = int(sys.argv[1])
 
-	# Calling exportar_dados function
+	# Chamando a função para exportar os dados
 	exportar_dados(dados_comarcas, tipo_arquivo)
